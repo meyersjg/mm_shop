2f30827de78752435c2ba8a085f6f41b2689ea85
John Meyers <meyersjg@miamioh.edu>
2014-02-05 15:31:55 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MmShop::Application.config.secret_key_base = 'b2d93a02969f36c69ea5c1804b2197ab484683fccb1d770b04170cb7158b5d3b92ac4a42727b10be2d57eeaa7bc626e4075132bea0b1048d172103f5b16b82d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MmShop::Application.config.session_store :cookie_store, key: '_mm_shop_session'
