5c006c77f08917a27d19559c947a9cbb04a8a82e
Amian87 <woodworks87@gmail.com>
2014-02-06 13:17:27 -0600

    Added Cart model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140205214941) do
+ActiveRecord::Schema.define(version: 20140206185535) do
+
+  create_table "carts", force: true do |t|
+    t.integer  "item_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "items", force: true do |t|
     t.string   "name"
